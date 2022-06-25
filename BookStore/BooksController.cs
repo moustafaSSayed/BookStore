b3c4cb90b0993ad507527660a50cf04f91a59176
Moustafa Sayed <Moustafa_201900818@fci.helwan.edu.eg>
2022-06-25 07:47:26 +0200

    add files

---
+++ BookStore/BooksController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class BooksController : Controller
+    {
+        private BookStoreDB db = new BookStoreDB();
+
+
+        // GET: UserOrderedBooks
+        public ActionResult Index()
+        {
+            var Books = db.Books;
+            return View(Books.ToList());
+        }
+        //to get the spicsific book 
+        private Book GetBook(int? id)
+        {
+            return (db.Books.Find(id));
+        }
+
+        public ActionResult CreateBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateBook(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+                return RedirectToAction("../Home/index");
+            }
+            return View(book);
+        }
+
+
+        // GET: Movies/Edit
+        public ActionResult EditBook(int? id)
+        {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                Book Book_search = GetBook(id);
+                if (Book_search == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(Book_search);
+        }
+
+
+        [HttpPost]
+        public ActionResult EditBook(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+
+                db.SaveChanges();
+                return RedirectToAction("../Home/index");
+            }
+            return View(book);
+        }
+
+        public ActionResult DeleteBook(int id)
+        {
+            return View(GetBook(id));
+        }
+
+
+        // POST: Books/Delete/5
+        [HttpPost , ActionName("DeleteBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteBookConfirm(int id)
+        {
+            db.Books.Remove(GetBook(id));
+            db.SaveChanges();
+            return RedirectToAction("../Home/Index");
+        }
+
+    }
+}
---
+++ BookStore/Controllers/UsersController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class UsersController : Controller
+    {
+        private BookStoreDB bookStoreDataBase = new BookStoreDB();
+
+        // GET: Users/UserSignUp
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        // POST: Users/USerSignUp
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                bookStoreDataBase.Users.Add(user);
+                bookStoreDataBase.SaveChanges();
+                Session["ID"] = user.userID;
+                Session["username"] = user.userFirstName.ToString();
+                return RedirectToAction("../Home/Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/EditUserInfo
+        public ActionResult EditUserInfo(int id)
+        {
+            return View(FindUser(id));
+        }
+
+        // POST: Users/EditUserInfo
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditUserInfo(User user, string userImage)
+        {
+            if (ModelState.IsValid)
+            {
+                user.userImage = CheckPhoto(user, userImage);
+                bookStoreDataBase.Entry(user).State = EntityState.Modified;
+                bookStoreDataBase.SaveChanges();
+                return RedirectToAction("../Home/Index");
+            }
+            return View(user);
+        }
+
+        //Get: Users/FindUser
+        public User FindUser(int id)
+        {
+            User user = bookStoreDataBase.Users.Find(id);
+            return user;
+        }
+
+        //GET: Users/CheckPhoto
+        public string CheckPhoto(User user,string userImage)
+        {
+            if (userImage != null)
+            {
+                return userImage;
+            }
+            else
+            {
+                return user.userImage;
+            }
+        }
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // Post: Login
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            try
+            {
+                var usr = bookStoreDataBase.Users.Single(u => u.userEmail == user.userEmail && u.userPassword == user.userPassword);
+                Session["ID"] = usr.userID;
+                Session["username"] = usr.userFirstName.ToString();
+                return RedirectToAction("../Books/Index");
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Username or Password is Wrong.");
+            }
+            return View(user);
+        }
+
+        public ActionResult MakeOrder(int bookID)
+        {
+            return RedirectToAction("../UserOrderedBooks/AddToCart/"+bookID);
+        }
+
+    }
+}
